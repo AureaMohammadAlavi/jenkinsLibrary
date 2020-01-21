d0ab6468e90b4f3f5a1e3c89e713a883e7bbfcc6
Mohammad Alavi <admin@smash1986.tk>
2020-01-21 15:15:01 +0300

    initial

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -1,7 +1,7 @@
 def call(String name="smash") {
   echo "Hello $name"
 
-  new File("sample.xml") <<"""
+  new File("sample-junit.xml") <<"""
   <?xml version="1.0" encoding="UTF-8"?>
 <testsuite name="me.smash.SomeClassTest" tests="1" skipped="0" failures="0" errors="0" timestamp="2020-01-21T12:11:17" hostname="MacBook-Pro.local" time="0.001">
   <properties/>
