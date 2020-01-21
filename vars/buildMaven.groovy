e3f27e454d2a13f8677b0ae9b59a6139ef42a9ba
Mohammad Alavi <admin@smash1986.tk>
2020-01-21 15:50:20 +0300

    initial

--- vars/buildMaven.groovy
+++ vars/buildMaven.groovy
@@ -1,5 +1,7 @@
 def call(Map config) {
-  git url: "https://github.com/AureaMohammadAlavi/$config.repository"
-  sh "./gradlew classes"
-  mail to: "m.alavi1986@yahoo.com", subject: "$config.repository", body: "$currentBuild.fullDisplayName ended in $currentBuild.currentResult"
+  node {
+    git url: "https://github.com/AureaMohammadAlavi/$config.repository"
+    sh "./gradlew classes"
+    mail to: "m.alavi1986@yahoo.com", subject: "$config.repository", body: "$currentBuild.fullDisplayName ended in $currentBuild.currentResult"    
+  }
 }
