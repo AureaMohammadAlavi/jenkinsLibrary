48b6f22ab70f8b10ec7dee79eb239cc74e269bb8
Mohammad Alavi <admin@smash1986.tk>
2020-01-21 14:19:45 +0300

    initial

--- src/me/smash/jenkins/MyLibrary.groovy
+++ src/me/smash/jenkins/MyLibrary.groovy
@@ -6,8 +6,26 @@ class MyLibrary {
     this.steps = steps
   }
 
+
+  def printAllMethods( obj ) {
+    if( !obj ){
+      println( "Object is null\r\n" )
+      return
+    }
+    if( !obj.metaClass && obj.getClass() ){
+      printAllMethods( obj.getClass() )
+      return
+    }
+    def str = "class ${obj.getClass().name} functions:\n"
+    obj.metaClass.methods.name.unique().each{
+      str += it+"(); "
+    }
+    println "${str}\n"
+  }
+  
   def printMessage(String msg) {
     steps.echo message: msg
-    steps.echo message: "${steps.getProperties()}"
+    printAllMethods(steps)
+
   }
 }
