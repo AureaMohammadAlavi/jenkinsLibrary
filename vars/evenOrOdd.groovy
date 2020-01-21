0729a83be4bd1bcc003129cf4ed19b0985db98f7
Mohammad Alavi <admin@smash1986.tk>
2020-01-21 17:08:41 +0300

    initial

--- vars/evenOrOdd.groovy
+++ vars/evenOrOdd.groovy
@@ -2,20 +2,24 @@ def call(int buildNumber) {
   if (buildNumber % 2 == 0) {
     pipeline {
       agent any
-      stages('even') {
-        steps {
-          echo "even build number $env.BUILD_NUMBER"
+      stages {
+        stage('even') {
+          steps {
+            echo "even build number $env.BUILD_NUMBER"
+          }
         }
       }
     }
-  } else {
-    pipeline {
-      agent any
-      stages('odd') {
-        steps {
-          echo "odd build number $env.BUILD_NUMBER"
+    } else {
+      pipeline {
+        agent any
+        stages {
+          stage('odd') {
+            steps {
+              echo "odd build number $env.BUILD_NUMBER"
+            }
+          }
         }
       }
     }
   }
-}
